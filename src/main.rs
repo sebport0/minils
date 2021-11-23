dd27df3c5953acac27920f8f660daf880a966881
Samuel Bassi <sebassi@live.com>
2021-11-23 00:05:41 -0300

    Check path is dir

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,16 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let path_to_read = args[1].clone();
-    let contents =
-        fs::read_dir(path_to_read).expect("Something went wrong when reading from path!");
+    let metadata = fs::metadata(&path_to_read).unwrap();
 
-    for path in contents {
-        println!("{}", path.unwrap().path().display());
+    if metadata.is_dir() {
+        let contents =
+            fs::read_dir(path_to_read).expect("Something went wrong when reading from path!");
+
+        for path in contents {
+            println!("{}", path.unwrap().path().display());
+        }
+    } else {
+        panic!("The path provided is not a directory.");
     }
 }
