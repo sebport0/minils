71265e5fe743b251ed055764cbd391d3e0bbe79f
Samuel Bassi <sebassi@live.com>
2021-11-28 23:11:53 -0300

    Refactor

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::error::Error;
+
+use chrono::{DateTime, Utc};
+use std::fs;
+use std::fs::DirEntry;
+use std::os::unix::fs::PermissionsExt;
+
+pub struct Config {
+    pub path_to_read: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 2 {
+            return Err("Missing read path argument.");
+        }
+        let path_to_read = args[1].clone();
+
+        Ok(Config { path_to_read })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents =
+        fs::read_dir(config.path_to_read).expect("Something went wrong when reading from path!");
+
+    for entry in contents {
+        print_entry(entry.unwrap())
+    }
+
+    Ok(())
+}
+
+pub fn print_entry(entry: DirEntry) {
+    if let Ok(metadata) = entry.metadata() {
+        let permissions = metadata.permissions().mode();
+        let size = metadata.len();
+
+        let created: DateTime<Utc> = metadata.created().unwrap().into();
+        let created = created.date();
+
+        let last_modified: DateTime<Utc> = metadata.modified().unwrap().into();
+        let last_modified = last_modified.date();
+
+        let last_accessed: DateTime<Utc> = metadata.accessed().unwrap().into();
+        let last_accessed = last_accessed.date();
+
+        let path = entry.path();
+        let path_last = path.iter().last().unwrap().to_str().unwrap();
+
+        println!(
+            "{} {} {} {} {} {}",
+            permissions, size, created, last_modified, last_accessed, path_last
+        );
+    } else {
+        panic!(
+            "Something went wrong when reading metadata for {:?}",
+            entry.path()
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,15 @@
-use chrono::{DateTime, Utc};
-use std::os::unix::fs::PermissionsExt;
-use std::{env, fs};
+use minils::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let path_to_read = args[1].clone();
-    let metadata = fs::metadata(&path_to_read).unwrap();
-
-    if metadata.is_dir() {
-        let contents =
-            fs::read_dir(path_to_read).expect("Something went wrong when reading from path!");
-
-        for entry in contents {
-            if let Ok(entry) = entry {
-                if let Ok(metadata) = entry.metadata() {
-                    let permissions = metadata.permissions().mode();
-                    let size = metadata.len();
-
-                    let created: DateTime<Utc> = metadata.created().unwrap().into();
-                    let created = created.date();
-
-                    let last_modified: DateTime<Utc> = metadata.modified().unwrap().into();
-                    let last_modified = last_modified.date();
-
-                    let last_accessed: DateTime<Utc> = metadata.accessed().unwrap().into();
-                    let last_accessed = last_accessed.date();
-
-                    let path = entry.path();
-                    let path_last = path.iter().last().unwrap().to_str().unwrap();
-
-                    println!(
-                        "{} {} {} {} {} {}",
-                        permissions, size, created, last_modified, last_accessed, path_last
-                    );
-                } else {
-                    panic!(
-                        "Something went wrong when reading metadata for {:?}",
-                        entry.path()
-                    );
-                }
-            }
-        }
-    } else {
-        panic!("The path provided is not a directory.");
+    if let Err(e) = minils::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
     }
 }
